72ffbf725f3ad9367dc4d73e90a09e90a7a90b86
Matt Olde <mattolde@gmail.com>
2015-02-12 18:20:05 +1100

    phantomjs setup

---
+++ phantomjs/phantomjs.js
@@ -0,0 +1,17 @@
+var phantom = require('phantom');
+
+phantom.create(function(ph){
+    ph.createPage(function(page){
+        // open page to be converted to PDF
+        page.open('http://www.google.com', function(status){
+            console.log('opened google?', status);
+
+            page.evaluate(function(){
+                return document.title;
+            }, function(result){
+                console.log('Page title is ', result);
+                ph.exit;
+            });
+        });
+    });
+})
