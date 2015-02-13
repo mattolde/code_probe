d88b2b92b5ef879815d9164ede31cf6e0c3ef188
Matt Olde <mattolde@gmail.com>
2015-02-13 11:55:14 +1100

    saving as PDF

--- phantomjs/phantomjs.js
+++ phantomjs/phantomjs.js
@@ -1,17 +1,30 @@
 var phantom = require('phantom');
 
+// Set PDF size to letter - 33.87 × 100.51 cm
+// phantom.paperSize = { format: 'Letter', orientation: 'portrait', margin: '1cm' };
+
 phantom.create(function(ph){
+
     ph.createPage(function(page){
+
         // open page to be converted to PDF
-        page.open('http://www.google.com', function(status){
+        page.open('http://phantomjs.org/api/webpage/property/paper-size.html', function(status){
+
             console.log('opened google?', status);
 
             page.evaluate(function(){
                 return document.title;
             }, function(result){
+
                 console.log('Page title is ', result);
+
+                page.render('test.pdf');
                 ph.exit();
+
             });
+
         });
+
     });
+
 });
