5242a901014094b516294ba489a89bce05888170
Matt Olde <mattolde@gmail.com>
2015-02-19 11:47:13 +1100

    map is being loaded

--- google-maps/googlemaps.js
+++ google-maps/googlemaps.js
@@ -1,12 +1,21 @@
 function init() {
+
+  // setup map options
   var mapOptions = {
-    center: { lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    // mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   var venueMap;
+  // Map() draws the map.
   venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 
+function loadScript() {
+  var script = document.createElement('script');
+  script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
+  document.body.appendChild(script);
+}
 
-google.maps.event.addDomListener(window, 'load', init);
+window.onload = loadScript;
