67496616492baeeed94c4a5868d981b9c87b9e4f
Matt Olde <mattolde@gmail.com>
2015-02-19 17:39:35 +1100

    adding marker to map

--- google-maps/googlemaps.js
+++ google-maps/googlemaps.js
@@ -1,19 +1,30 @@
 function init() {
 
+  var latLng = new google.maps.LatLng(-34.397, 150.644);
+
   // setup map options
   var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
+    center: new google.maps.LatLng(latLng),
     zoom: 8,
     // mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
-  var venueMap;
+  var map;
   // Map() draws the map.
-  venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    title: 'this is a test marker'
+  });
+
+  marker.setMap(map);
 }
 
 function loadScript() {
   var script = document.createElement('script');
+  // use JSONP with API
   script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
   document.body.appendChild(script);
 }
