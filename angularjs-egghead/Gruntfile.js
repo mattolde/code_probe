871f5fd460d180ecd58d36da030d45aef8e4db86
Matt Olde <mattolde@gmail.com>
2015-03-04 15:00:35 +1100

    grunt setup

---
+++ angularjs-egghead/Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    watch: {
+      options: {
+        livereload: true,
+        files: ['**/*']
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+};
